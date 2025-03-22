9f47465002a1d630e33b0e72b41daff0d5d8d626
Mblender3d <mikublender3d@gmail.com>
2025-03-22 18:51:58 +0900

    update router

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
+<BrowserRouter basename={process.env.PUBLIC_URL}>
+    <div className="App">
         <div>
       <Routes>
-        <Route path="snsapp" element={<Home />} />
-        <Route path="snsapp/signin" element={<Signin />} />
-        <Route path="snsapp/signup" element={<Signup />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
       </div>
