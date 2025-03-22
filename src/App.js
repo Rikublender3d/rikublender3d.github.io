3379d1012bcd9a7ca42f67f715dca0dfe7df79b6
Mblender3d <mikublender3d@gmail.com>
2025-03-22 19:48:55 +0900

    update router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
-const APP_NAME = "snsapp"; // これが、リポジトリ名
 function App() {
   return (
 <BrowserRouter>
     <div className="App">
         <div>
       <Routes>
-        <Route path={APP_NAME + '/' }element={<Home />} />
-        <Route path={APP_NAME + '/signin'} element={<Signin />} />
-        <Route path={APP_NAME + '/signup'} element={<Signup />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
     </div>
       </div>
