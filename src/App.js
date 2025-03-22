c40fecb50060f988b3a7e782fa1cefd89d07ccde
Mblender3d <mikublender3d@gmail.com>
2025-03-22 22:50:35 +0900

    inital

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 function App() {
   return (
-<HashRouter  basename="/snsapp">
+<HashRouter>
     <div className="App">
         <div>
       <Routes>
-        <Route path='' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/signin' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
       </Routes>
