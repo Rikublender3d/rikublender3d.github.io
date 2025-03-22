67bc9753bd7deed2b260007d0097c70ddcc5c9e5
Mblender3d <mikublender3d@gmail.com>
2025-03-22 22:47:00 +0900

    inital commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
         <div>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='' element={<Home />} />
         <Route path='/signin' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
       </Routes>
